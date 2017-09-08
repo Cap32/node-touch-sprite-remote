631f4f2cb19245c91d93bcf9908a7dcec48f2ec4
3went2 <webb.png@gmail.com>
2017-09-08 19:20:41 +0800

    update testing

--- test/test.js
+++ test/test.js
@@ -1,25 +1,56 @@
 
-import { fetchAuth, getDeviceName, run } from '../src';
+import { fetchAuth, getDeviceName, run, upload } from '../src';
+import mockDevice from './fixtures/mockDevice';
+import { join } from 'path';
 
-test('fetchAuth', async () => {
-	jest.setTimeout(30000);
-	const res = await fetchAuth({
-		devices: [],
-		key: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-		valid: 3600,
+describe('auth', () => {
+	test('fetchAuth', async () => {
+		jest.setTimeout(30000);
+		const res = await fetchAuth({
+			devices: [],
+			key: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+			valid: 3600,
+		});
+		expect(res.status).toBe(401);
 	});
-	expect(res.status).toBe(401);
 });
 
-test('getDeviceName', async () => {
-	const res = await getDeviceName('http://192.168.199.126:50005');
-	console.log('res', res);
-	// expect(!!lib).toBe(true);
-});
+describe('devices', () => {
+	let deviceA;
+	let deviceB;
 
-test('run', async () => {
-	const res = await run('http://192.168.199.126:50005', {
-		auth: 'asdf',
+	beforeEach(async () => {
+		deviceA = mockDevice(3001);
+		deviceB = mockDevice(3002);
+		await deviceA.start();
+		await deviceB.start();
 	});
-	expect(res.trim()).toBe('ok');
+
+	afterEach(async () => {
+		await deviceA.stop();
+		await deviceB.stop();
+	});
+
+	test('getDeviceName', async () => {
+		const res = await getDeviceName(deviceA.url);
+		expect(res).toBe('hello');
+	});
+
+	test('run', async () => {
+		const res = await run(deviceA.url, {
+			auth: 'asdf',
+		});
+		expect(res.trim()).toBe('ok');
+	});
+
+	test('upload', async () => {
+		const res = await upload(deviceB.url, {
+			file: join(__dirname, 'fixtures/res.jpg'),
+			type: 'res',
+			clientFile: '/fork.png',
+			auth: 'asdf',
+		});
+		expect(res.trim()).toBe('ok');
+	});
+
 });
